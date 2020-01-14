817fcc3cb6e0ca7d25b63dc27fbbdafefb18e258
Elias AACHACH <elaachac@e1r12p13.42.fr>
2020-01-14 11:53:03 +0100

    1st push, some things to work at home2.0

--- ft_strlen_to_n.c
+++ ft_strlen_to_n.c
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   ft_strlen_to_n.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elaachac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/11 20:54:13 by elaachac          #+#    #+#             */
-/*   Updated: 2020/01/14 11:52:53 by elaachac         ###   ########.fr       */
+/*   Created: 2020/01/14 11:46:10 by elaachac          #+#    #+#             */
+/*   Updated: 2020/01/14 11:48:55 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-int		get_next_line(int fd, char **line)
+int		ft_strlen_to_n(char *s)
 {
-	static char		*rest;
-	char			*tmp;
-	char			tab[BUFFER_SIZE + 1];
-	int				ret;
-	int				i;
-	int				y;
+	int count;
 
-	i = 0;
-	y = 0;
-	rest = NULL;
-	while ((ret = read (fd, tab, BUFFER_SIZE)) > 0)
+	count = 0;
+	while(s)
 	{
-		tab[BUFFER_SIZE] = '\0';
-		if (rest)
-			if (!(tmp = ft_strjoin(rest, tab)))
-				return(-1);
-		else
-			if (!(tmp = ft_strdup(tab)))
-				return (-1);
-
-		
+		if (s[count] == '\n')
+			return (count);
+		s[count++]
 	}
-	return (0);
+	return (-1);
 }
