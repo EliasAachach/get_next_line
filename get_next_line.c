59420f5c43b461a53b99a36ce45c18d3dee9ab27
EliasAachach <elaachac@student.42.fr>
2020-03-05 00:14:57 +0100

    new gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 20:54:13 by elaachac          #+#    #+#             */
-/*   Updated: 2020/02/21 14:35:17 by elaachac         ###   ########.fr       */
+/*   Updated: 2020/03/05 00:14:22 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,32 @@ void ft_free(t_gnl *gnl, char **line, int free_line)
 	}
 }
 
+static	int	get_line(char **rest, char **line, size_t start)
+{
+	char *tmp;
+
+	if ((tmp = ft_strchr(rest)))
+}
+
+static	int	read_fd(int fd, char **line, char **rest)
+{
+	char	*buf;
+	int		total_len;
+	int		len;
+
+	if (!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+		//////
+	while ((gnl.ret = read(fd, buf, BUFFER_SIZE)))
+	{
+		*rest = ft_realloc(*rest, total_len + len + 1, total_len);
+		ft_memcpy(*rest + total_len, buf, len);
+		*rest[total_len + len] = '\0';
+		if (get_line(rest, line, total_len))
+			return (ft_free(&gnl, line, 0));
+	}
+}
+
 int	get_next_line(int fd, char **line)
 {
 	static char	*rest = NULL;
@@ -68,88 +94,10 @@ int	get_next_line(int fd, char **line)
 	gnl.j = 0;
 	gnl.pos = 0;
 	gnl.ret = 0;
-	buf[BUFFER_SIZE] =  '\0';
 	if (line == NULL || BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
 		return (-1);
 	while ((gnl.pos = ft_is_endl(gnl.str)) == -1 && ((gnl.ret = read(fd, buf, BUFFER_SIZE)) > 0))
 	{
-		gnl.len += gnl.ret;
-		if (!gnl.str)
-		{
-			if ((gnl.str = ft_strdup(buf)) == NULL)
-			{
-				ft_free(&gnl, line, 0);
-				return (-1);
-			}
-		}
-		else
-		{
-			if ((gnl.str = ft_strjoin(gnl.tmp, buf)) == NULL)
-			{
-				ft_free(&gnl, line, 0);
-				return (-1);
-			}
-		}
-		if ((gnl.tmp = ft_strdup(gnl.str)) == NULL)
-		{
-			ft_free(&gnl, line, 0);
-			return (-1);
-		}
-	}
-	if((gnl.tmp = ft_substr(buf, 0, gnl.pos)) == NULL)
-		{
-			ft_free(&gnl, line, 0);
-			return (-1);
-		}
-	if ((gnl.str = ft_strdup(gnl.tmp)) == NULL)
-	{
-		ft_free(&gnl, line, 0);
-		return (-1);
-	}
-	if (!(*line = (char *)malloc(sizeof(char) * (gnl.len + 1))))
-	{
-		ft_free(&gnl, line, 0);
-		return (-1);
-	}
-	if (gnl.ret == 0)
-	{
-		ft_free(&gnl, line, 1);
 		return (0);
 	}
-	if (rest)
-	{
-		while (gnl.i <= ft_strlen(rest))
-		{
-			line[0][gnl.i] = rest[gnl.i];
-			gnl.i++;
-		}
-		gnl.j = gnl.i;
-		free(gnl.tmp);
-		if ((gnl.tmp = ft_substr(rest, gnl.i, (ft_strlen(rest) - gnl.i))) == NULL)
-		{
-			return (-1);
-		}
-		free(rest);
-	}
-	if (!(rest = (char *)malloc(sizeof(char) * (gnl.len - gnl.pos + 1))))
-	{
-		ft_free(&gnl, line, 0);
-		return (-1);
-	}
-	while (gnl.i < gnl.len && gnl.pos != 1)
-	{
-		line[0][gnl.i] = gnl.str[gnl.j];
-		gnl.i++;
-		gnl.j++;
-	}
-	line[0][gnl.i] = '\0';
-	gnl.j = 0;
-	while(gnl.pos <= gnl.len && buf[gnl.pos])
-	{
-		rest[gnl.j] = buf[gnl.pos];
-		gnl.pos++;
-		gnl.j++;
-	}
-	rest[gnl.j] = '\0';
-	return (1);
 }
